3da2be6c2aef141be99fd335e8402bca9af813be
TahaT.Unal <tahaunal@uludag.edu.tr>
2021-10-14 23:53:46 +0300

    Third Commit

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        
+        //list in which the number and order of list elements is read-only
+        Task<IReadOnlyList<Product>> GetProductAsync();
+    }
+}
